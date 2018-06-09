502d65f98b80f1595583359570aa4cdf24dc9c59
Sami Kerola <kerolasa@iki.fi>
2018-06-09 21:54:27 +0100

    make function declaration to match with type requirements
    This fixes following warning.
    
    conserver/group.c: In function ‘CheckPass’:
    conserver/group.c:711:15: warning: assignment to ‘int (*)(int,  const struct
      pam_message **, struct pam_response **, void *)’ from incompatible pointer
      type ‘int (*)(int,  struct pam_message **, struct pam_response **, void *)’
      [-Wincompatible-pointer-types]
         conv.conv = &QuietConv;

--- conserver/group.c
+++ conserver/group.c
@@ -638,7 +638,7 @@ DestroyGroup(GRPENT *pGE)
 
 #if HAVE_PAM
 int
-QuietConv(int num_msg, struct pam_message **msg,
+QuietConv(int num_msg, const struct pam_message **msg,
 	  struct pam_response **resp, void *appdata_ptr)
 {
     int i;
