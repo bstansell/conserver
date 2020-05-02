785f2dae9a427722511cd22e582be377ab1afa8c
Bryan Stansell <bryan@conserver.com>
2020-05-01 20:46:59 -0700

    Release v8.2.5

--- conserver/version.h
+++ conserver/version.h
@@ -12,9 +12,9 @@
 @(#) Copyright 2000 conserver.com.\n\
 All rights reserved.\n"
 
-#define VERSION_DATE	"2019/03/26"
+#define VERSION_DATE	"2020/05/01"
 #define VERSION_MAJOR	8
 #define VERSION_MINOR	2
-#define VERSION_REV	4
+#define VERSION_REV	5
 #define VERSION_TEXT	"conserver.com version"
 #define VERSION_UINT	(VERSION_MAJOR * 1000000 + VERSION_MINOR * 1000 + VERSION_REV)
